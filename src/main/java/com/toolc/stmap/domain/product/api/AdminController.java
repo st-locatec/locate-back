9a728abb7810548ceaf078ca32318f61b6b76579
JisooPark27 <jspark9812@gmail.com>
2021-09-27 20:48:56 +0900

    fix : get Registered http request method

--- src/main/java/com/toolc/stmap/domain/product/api/AdminController.java
+++ src/main/java/com/toolc/stmap/domain/product/api/AdminController.java
@@ -7,6 +7,7 @@ import com.toolc.stmap.global.response.SuccessResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,7 +22,7 @@ public class AdminController {
 
   private final InquiryProduct inquiryProduct;
 
-  @PostMapping("/api/admin/registered")
+  @GetMapping("/api/admin/registered")
   public ResponseEntity<?> getRegistered() {
 
     List<Product> products = inquiryProduct.inquiry(true);
@@ -32,7 +33,7 @@ public class AdminController {
     return ResponseEntity.ok().body(new SuccessResponse(products));
   }
 
-  @PostMapping("/api/admin/NotRegistered")
+  @GetMapping("/api/admin/NotRegistered")
   public ResponseEntity<?> getNotRegistered() {
 
     List<Product> products = inquiryProduct.inquiry(false);
